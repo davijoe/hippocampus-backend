3653b3e48a201ec5039ecf9f399bef42b725c4e4
davijoe <danx8076@stud.kea.dk>
2023-12-01 05:12:31 +0100

    Mange story points

---
+++ src/main/java/memgrp/memorize/dto/ValueRequest.java
@@ -0,0 +1,19 @@
+package memgrp.memorize.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ValueRequest {
+
+    String valueDescription;
+    String valueName;
+
+    public ValueRequest(String valueDescription, String valueName){
+        this.valueDescription = valueDescription;
+        this.valueName = valueName;
+    }
+}
---
+++ src/test/java/memgrp/memorize/MemorizeApplicationTests.java
@@ -0,0 +1,12 @@
+package memgrp.memorize;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemorizeApplicationTests {
+
+    // @Test
+    // void contextLoads() {
+    // }
+
+}
